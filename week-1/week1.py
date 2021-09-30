ccebe2732a231a48358921fd2e6908978349e20b
antz22 <anthonyznj@gmail.com>
2021-09-30 18:18:03 -0400

    add week1

---
+++ week-1/week1.py
@@ -0,0 +1,20 @@
+# Data types include boolean, int, string, and float
+# Variables are created in python using the name of the variable, the equal sign, and the variable's value.
+
+# A string value is assigned to the variable named "name"
+name = "Bob"
+
+# An int value is assigned to the variable named "pennies"
+pennies = 52
+
+# Arrays are a special data type that allows you to store a list of variables, initialized using square brackets and the values
+names = ["Bob", "John", "Jill"]
+
+# To access values in an array, we perform array indexing. The first value of an array starts at index 0!
+
+# "Bob"
+names[0]
+# "John"
+names[1]
+# "Jill"
+names[2]
